15d00618ccda8180adfe43d8e79c2691e96bd64e
Ed Jannoo <ed.jannoo@devability.co.uk>
2017-05-10 08:53:31 +0100

    Fix the unit test

--- app/uk/gov/hmrc/apiplatformtest/controllers/HelloController.scala
+++ app/uk/gov/hmrc/apiplatformtest/controllers/HelloController.scala
@@ -17,37 +17,38 @@
 package uk.gov.hmrc.apiplatformtest.controllers
 
 import play.api.libs.json.Json
+import play.api.mvc.Action
 import uk.gov.hmrc.play.http.HeaderCarrier
 import uk.gov.hmrc.play.microservice.controller.BaseController
 
 import scala.concurrent.Future
 
-trait HelloController extends BaseController with HeaderValidator {
+trait HelloController extends BaseController {
   implicit val hc: HeaderCarrier
 
   val response = Ok(Json.toJson("""{ "message": "Hello World" }"""))
 
-  def get = validateAccept(acceptHeaderValidationRules).async {
+  def get = Action.async {
     Future.successful(response)
   }
 
-  def getWithOneParam(param: String) = validateAccept(acceptHeaderValidationRules).async {
+  def getWithOneParam(param: String) = Action.async {
     Future.successful(response)
   }
 
-  def getWithTwoParams(param1: String, param2: String) = validateAccept(acceptHeaderValidationRules).async {
+  def getWithTwoParams(param1: String, param2: String) = Action.async {
     Future.successful(response)
   }
 
-  def post = validateAccept(acceptHeaderValidationRules).async {
+  def post = Action.async {
     Future.successful(response)
   }
 
-  def postWithOneParam(param: String) = validateAccept(acceptHeaderValidationRules).async {
+  def postWithOneParam(param: String) = Action.async {
     Future.successful(response)
   }
 
-  def postWithTwoParams(param1: String, param2: String) = validateAccept(acceptHeaderValidationRules).async {
+  def postWithTwoParams(param1: String, param2: String) = Action.async {
     Future.successful(response)
   }
 
