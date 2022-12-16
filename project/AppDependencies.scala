7dcec1b657bd8377c0452fe27f7080157ca593b2
Matt Clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2022-12-16 09:22:10 +0000

    API-5591: Addressing Pr Bot Comment

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,8 +12,7 @@ object AppDependencies {
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-28"        % bootstrapVersion,
     "com.typesafe.play"             %% "play-json"                        % "2.9.2",
     "com.typesafe.play"             %% "play-json-joda"                   % "2.9.2",
-    "uk.gov.hmrc"                   %% "domain"                           % "8.1.0-play-28",
-    "uk.gov.hmrc"                   %% "logback-json-logger"              % "5.2.0"
+    "uk.gov.hmrc"                   %% "domain"                           % "8.1.0-play-28"
   )
 
   private lazy val test = Seq(
